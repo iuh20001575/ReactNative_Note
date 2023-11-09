39bcceb3431055974c4b196eef971c33bae895ff
Hà Anh Thảo <thaoanhhaa1@gmail.com>
2023-11-09 19:28:46 +0700

    Update API BE

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,6 +1,6 @@
 const configs = {
     ENDPOINT_DEV: 'http://localhost:8080',
-    ENDPOINT_PRO: 'https://reactnative-note-json-server.onrender.com',
+    ENDPOINT_PRO: 'https://react-native-note-json-server.vercel.app',
 };
 
 export default configs;
